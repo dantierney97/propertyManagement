ab9e13354008135676c53f30f088dae2d2edde89
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-10 16:04:40 +0000

    Update mongo.js
    Added connectMongo function to test connection to the MongoDB

---
+++ db/mongo.js
@@ -0,0 +1,25 @@
+// Import the Mongoose Library (used to interact with MongoDB)
+const mongoose = require("mongoose");
+
+// Load environment variables from the .env file
+require ("dotnet").config();
+
+/**
+ * Function to connect to MongoDB using the given URI in .env
+ */
+const connectMongo = async () => {
+    try {
+        // Attempt to connect using mongoose
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser:    true,          // Ensures compatibility with modern MongoDB Versions
+            useUnifiedTopology: true,       // Use the new server discovery and monitoring engine
+        });
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.error("MongoDB Connection Error: ", error.message);
+    }
+};
+
+// Export the connectMongo function
+module.exports = connectMongo;
