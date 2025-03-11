d1a45eef23dbc7cdea26162c48c86dfdffffea65
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-11 22:25:46 +0000

    Update mongo.js
    Added an exit process in the event of an error when connecting to mongoDB

--- db/mongo.js
+++ db/mongo.js
@@ -15,6 +15,7 @@ const connectMongo = async () => {
     }
     catch (error) {
         console.error("MongoDB Connection Error: ", error.message);
+        process.exit(1); // Exit process if the connection fails
     }
 };
 
