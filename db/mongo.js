f1a4fd32bd33306afd64f0bda5c91abff10a86dd
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-11 11:43:31 +0000

    Update mongo.js
    Removed depreciated options from MongoDB connection

--- db/mongo.js
+++ db/mongo.js
@@ -10,10 +10,7 @@ require ("dotenv").config();
 const connectMongo = async () => {
     try {
         // Attempt to connect using mongoose
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser:    true,          // Ensures compatibility with modern MongoDB Versions
-            useUnifiedTopology: true,       // Use the new server discovery and monitoring engine
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     }
     catch (error) {
