d9c09c48df947041932a2307d09eb5d630567c45
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-11 20:00:43 +0000

    Updated userRoutes.js
    Added route for searching for a user by email address

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 // Import modules
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, getUserByEmail } = require("../controllers/userController");
 
 const router = express.Router();    // Create a new Express Router instance
 
@@ -8,6 +8,11 @@ const router = express.Router();    // Create a new Express Router instance
  * Route to create new user
  */
 router.post("/users", createUser);
+
+/**
+ * Route to get user by email search
+ */
+router.post("/users", getUserByEmail);
 
 // Output sucessful loading to console
 console.log("userRoutes Loaded Successfully!");
