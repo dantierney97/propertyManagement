3e51db5ad887886a849513639edaf765027acc08
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-11 11:33:43 +0000

    Update server.js
    Added main server loop to call DB connection functions

---
+++ server.js
@@ -0,0 +1,38 @@
+/**
+ * Main Entry Point for Server
+ */
+
+// Module Imports
+const express = require('express');                     // Express.js for handling web requests
+const {connectPostgres} = require("./db/postgres");     // Import Postgres connection function
+const connectMongo = require("./db/mongo");             // Import MongoDB connection function
+const { connect } = require('mongoose');
+
+// Initialise Express
+const app = express();
+const port = 3000;      // Set the server port
+
+// Setup Middleware to parse json
+app.use(express.json());
+
+/**
+ * Main Application
+ */
+
+const startServer = async () => {
+
+    console.log("Starting Smart Property Management Backend...");   // Output start of startup
+
+    // Attempt connection to both databases
+    await connectPostgres();            // Test connection to the Postgres server
+    await connectMongo();               // Test connection to the MongoDB server
+
+    // Start Express server
+    app.listen(port, () => {
+        console.log(`Express Server running on http://localhost:${port}`);
+    });
+
+};
+
+// Run Main Loop
+startServer();
