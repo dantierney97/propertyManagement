109ac53ba697780135b7d4b573887b03208c5288
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-12 00:16:58 +0000

    Update maintenanceRequest.js
    Updated maintenanceRequest schema to account for an assigned contractor

--- models/maintenanceRequest.js
+++ models/maintenanceRequest.js
@@ -4,19 +4,20 @@
 
 // Schema for maintenance update; this will be embedded in the main maintenance request
 const MaintenanceUpdateSchema = new mongoose.Schema ({
-    updated_by: {type: String, ref: "user", required: true},
-    description: {type: String, required: true},
-    timestamp:  {type: Date, default: Date.now}
+    updated_by:     {type: String, ref: "user", required: true},
+    description:    {type: String, required: true},
+    timestamp:      {type: Date, default: Date.now}
 });
 
 // Schema for maintenance requests
 const MaintenanceRequestSchema = new mongoose.Schema ({
-    property_id: {type: String, ref: "Property", required: true},
-    tenant_id: {type: String, ref: "Tenant", required: true},
-    description: {type: String, required: true},
-    status: {type: String, enum: ["Open", "In Progress", "Closed"], default: "Open"},
-    created_at: {type: Date, default: Date.now},
-    updates: [MaintenanceUpdateSchema] // Updates are embedded
+    property_id:    {type: String, ref: "Property", required: true},
+    tenant_id:      {type: String, ref: "Tenant", required: true},
+    description:    {type: String, required: true},
+    status:         {type: String, enum: ["Open", "In Progress", "Closed"], default: "Open"},
+    assigned_to:    {type: String, ref: "Contractor", default: null},
+    created_at:     {type: Date, default: Date.now},
+    updates:        [MaintenanceUpdateSchema] // Updates are embedded
 });
 
 // Create the Mongoose Model
