16662a7aaf68648d00ade2f63da287a577ec96c2
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-13 08:59:54 +0000

    Added testing suite, removed node_modules folder from tracking

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+/**
+ * 🚀 Main Test Runner
+ * -------------------
+ * This script initializes testing by calling specific test files.
+ */
+
+require("./tests/testMaintenanceRequests"); // Import and execute maintenance request tests
