74d543e663c70f463080039473a27cfe0ebb0716
Daniel Tierney <daniel.tierney1@icloud.com>
2025-03-13 09:53:53 +0000

    Update maintenanceRoutes.js
    Updated routes to include new delete function

--- routes/maintenanceRoutes.js
+++ routes/maintenanceRoutes.js
@@ -7,30 +7,36 @@ const { createMaintenanceRequest, addMaintenanceUpdate, assignContractor, getAll
 const router = express.Router();
 
 /**
- * @route   POST /api/maintenance/create
- * @desc    Create a new maintenance request
- * @access  Public (Authentication to be added later)
+ * @route       POST /api/maintenance/create
+ * @desc        Create a new maintenance request
+ * @access      Public (Authentication to be added later)
  */
 router.post("/create", createMaintenanceRequest);
 
 /**
- * @route   PUT /api/maintenance/update
- * @desc    Add an update to an existing maintenance request
- * @access  Public (Authentication to be added later)
+ * @route       POST /api/maintenance/deleteMaintenanceRequest
+ * @desc        Deletes an exisiting maintenance request
+ * @access      Public (Authentication to be added later)
+ */
+
+/**
+ * @route       PUT /api/maintenance/update
+ * @desc        Add an update to an existing maintenance request
+ * @access      Public (Authentication to be added later)
  */
 router.put("/update", addMaintenanceUpdate);
 
 /**
- * @route   PUT /api/maintenance/assignContractor
- * @desc    Assign a contractor to an existing maintenance request
- * @access  Public (Authentication to be added later)
+ * @route       PUT /api/maintenance/assignContractor
+ * @desc        Assign a contractor to an existing maintenance request
+ * @access      Public (Authentication to be added later)
  */
 router.put("/assignContractor", assignContractor);
 
 /**
- * @route   POST /api/maintenance/getRequestsByProperty
- * @desc    Retrieves all maintenance requests for a given property regardless of status
- * @access  Public (Authentication to be added later)    
+ * @route       POST /api/maintenance/getRequestsByProperty
+ * @desc        Retrieves all maintenance requests for a given property regardless of status
+ * @access      Public (Authentication to be added later)    
  */
 router.post("/getRequestsByProperty", getRequestsByProperty);
 
